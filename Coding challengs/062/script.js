183e11f25e04d09c08f7d80bcabed834a0c291fe
Matei Ungureanu <mateiungureanu8@gmail.com>
2022-06-30 14:42:01 +0300

    089 - 095

--- Coding challengs/062/script.js
+++ Coding challengs/062/script.js
@@ -13,7 +13,7 @@ const data2 = [12, 5, -5, 0, 4];
 console.log(`...${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
 
 const printForecast = function (arr) {
-  let str = 0;
+  let str = '';
   for (let i = 0; i < arr.length; i++) {
     str += `${arr[i]}ºC in ${i + 1} days ...`;
   }
@@ -21,3 +21,4 @@ const printForecast = function (arr) {
 };
 
 printForecast(data1);
+printForecast(data2);
