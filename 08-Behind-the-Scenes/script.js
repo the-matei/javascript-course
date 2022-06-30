8b5519a8bce881a4f58e4f61d7ee0dc7837d98a8
Matei Ungureanu <mateiungureanu8@gmail.com>
2022-06-30 14:41:34 +0300

    089 - 095

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -38,7 +38,7 @@ calcAge(1991);
 // console.log(age);
 // printAge();
 
-
+*/
 ///////////////////////////////////////
 // Hoisting and TDZ in Practice
 
@@ -47,9 +47,9 @@ console.log(me);
 // console.log(job);
 // console.log(year);
 
-var me = 'Jonas';
-let job = 'teacher';
-const year = 1991; 
+var me = "Jonas";
+let job = "teacher";
+const year = 1991;
 
 // Functions
 console.log(addDecl(2, 3));
@@ -74,145 +74,5 @@ if (!numProducts) deleteShoppingCart();
 var numProducts = 10;
 
 function deleteShoppingCart() {
-  console.log('All products deleted!');
+  console.log("All products deleted!");
 }
-
-var x = 1;
-let y = 2;
-const z = 3;
-
-console.log(x === window.x);
-console.log(y === window.y);
-console.log(z === window.z);
-
-
-///////////////////////////////////////
-// The this Keyword in Practice
-console.log(this);
-
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-calcAge(1991);
-
-const calcAgeArrow = birthYear => {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-calcAgeArrow(1980);
-
-const jonas = {
-  year: 1991,
-  calcAge: function () {
-    console.log(this);
-    console.log(2037 - this.year);
-  },
-};
-jonas.calcAge();
-
-const matilda = {
-  year: 2017,
-};
-
-matilda.calcAge = jonas.calcAge;
-matilda.calcAge();
-
-const f = jonas.calcAge;
-f();
-
-
-const jonas = {
-  firstName: 'Jonas',
-  year: 1991,
-  calcAge: function () {
-    // console.log(this);
-    console.log(2037 - this.year);
-
-    // Solution 1
-    // const self = this; // self or that
-    // const isMillenial = function () {
-    //   console.log(self);
-    //   console.log(self.year >= 1981 && self.year <= 1996);
-    // };
-
-    // Solution 2
-    const isMillenial = () => {
-      console.log(this);
-      console.log(this.year >= 1981 && this.year <= 1996);
-    };
-    isMillenial();
-  },
-
-  greet: () => {
-    console.log(this);
-    console.log(`Hey ${this.firstName}`);
-  },
-};
-jonas.greet();
-jonas.calcAge();
-
-// arguments keyword
-const addExpr = function (a, b) {
-  console.log(arguments);
-  return a + b;
-};
-addExpr(2, 5);
-addExpr(2, 5, 8, 12);
-
-var addArrow = (a, b) => {
-  console.log(arguments);
-  return a + b;
-};
-addArrow(2, 5, 8);
-
-
-let age = 30;
-let oldAge = age;
-console.log(age);
-console.log(oldAge);
-
-const me = {
-  name: "Jonas",
-  age: 30,
-};
-
-const friend = me;
-friend.age = 27;
-console.log("Friend:", friend);
-console.log("Me", me);
-
-
-
-let lastName = "Williams";
-let oldLastName = lastName;
-lastName = "Davis";
-console.log(lastName, oldLastName);
-
-const jessica = {
-  firstName: "Jessica",
-  lastName: "Williams",
-  age: 27,
-};
-
-const marriedJessica = jessica;
-console.log("Before marriage:", jessica);
-marriedJessica.lastName = "Davis";
-console.log("After marriage:", marriedJessica);
-
-const jessica2 = {
-  firstName: "Jessica",
-  lastName: "Williams",
-  age: 27,
-  family: ["Alice", "Bob"],
-};
-
-const jessicaCopy = Object.assign({}, jessica2);
-jessicaCopy.lastName = "Davis";
-
-jessicaCopy.family.push("Mary");
-jessicaCopy.family.push("Jhon");
-
-console.log("Before marriage:", jessica2);
-console.log("After marriage:", jessicaCopy);
-*/
