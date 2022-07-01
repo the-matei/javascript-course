a2563cf94c2613e341cfb5e6398520d18a580b46
Matei Ungureanu <mateiungureanu8@gmail.com>
2022-07-01 20:25:07 +0300

    this keywoed

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /*
 function calcAge(birthYear) {
@@ -38,7 +38,7 @@ calcAge(1991);
 // console.log(age);
 // printAge();
 
-*/
+
 ///////////////////////////////////////
 // Hoisting and TDZ in Practice
 
@@ -76,3 +76,86 @@ var numProducts = 10;
 function deleteShoppingCart() {
   console.log("All products deleted!");
 }
+
+
+// console.log(this);
+const obj = {
+  a: 32,
+};
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  // console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  // console.log(this);
+};
+calcAgeArrow(1980);
+
+const john = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+john.calcAge(1991);
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = john.calcAge;
+matilda.calcAge();
+
+const f = john.calcAge;
+f();
+*/
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+jonas.greet();
+jonas.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
