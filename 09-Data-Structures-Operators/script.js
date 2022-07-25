1b00d9c3384cde79325da43812ccef37db34ee68
Matei Ungureanu <mateiungureanu8@gmail.com>
2022-07-25 16:36:24 +0300

    Short circuiting (&& - ||)

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -42,6 +42,36 @@ const restaurant = {
   },
 };
 
+///////////////////////////////////////
+// Short circuiting (&& - ||)
+console.log("---- OR ----");
+console.log(3 || "Jonas");
+console.log("" || "Jonas");
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || "" || "Hello" || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log("---- AND ----");
+console.log(0 && "Jonas");
+console.log(7 && "Jonas");
+
+console.log("Hello" && 23 && null && "jonas");
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza("mushrooms", "spinach");
+}
+
+restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinach");
+
+/*
 ////////////////////////////////
 // Rest pattern and parameters
 const arr = [1, 2, ...[3, 4]];
@@ -75,7 +105,6 @@ console.log(x);
 restaurant.orderPizza("mushrooms", "onion", "olives", "spinach");
 restaurant.orderPizza("mushrooms");
 
-/*
 / ////////////////////////
 ////The Spread operator - ...
 const arr = [7, 8, 9];
